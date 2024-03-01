4a3d5a2502c94c5e4108b4060c949f9a737e38fc
migena <migena.allushi1@gmail.com>
2024-03-01 11:35:43 +0100

    decide player turn and symbol to put

--- javascript.js
+++ javascript.js
@@ -16,6 +16,8 @@ playBtn.onclick = function(){
 closeBtn.onclick = function(){
     dialog.close();
 }
+
+let playerNames = [];
 form.addEventListener('submit', function(e){
     e.preventDefault();
         
@@ -26,20 +28,40 @@ form.addEventListener('submit', function(e){
     const player1 = createPlayer(player1Name, 'X');
     const player2 = createPlayer(player2Name, 'O');
 
+    playerNames.push(player1);
+    playerNames.push(player2);
+
     dialog.close();
 });
 
-//create gameboard
+console.log(playerNames[1]);  // Access player1
+console.log(playerNames[0]);  // Access player2
+
+//logic to decide, put symbol and to prevent collision
+const putSymbol = () => {
+    //if its player1 turn put textcontent X, else put O
+    return (decideTurn() === playerNames[0]) ? button.textContent = playerNames[0].symbol : button.textContent = playerNames[1].symbol;
+}
+
+//logic to decide turn
+let playerTurn = playerNames[0];
+const decideTurn = () => {
+    //if player1 plays, then switch to player2 turn
+    return (playerTurn === playerNames[0])? playerNames[1] : playerNames[0];
+}
+
+//create gameboard and draw the gameboard to ui
+let button;
 const gameboard = (function(){
     const board = [];
     const row = 3;
     const column = 3;
     for(i = 0; i < row; i++){
         for(j = 0; j < column; j++){
-            const button = document.createElement('button');
+            button = document.createElement('button');
             button.classList.add('board-button');
             button.textContent = '';
-            //button.addEventListener('click', putSymbol);
+            button.addEventListener('click', putSymbol);
             board.push(button);
             boardContainer.appendChild(button);
         }
@@ -49,12 +71,8 @@ const gameboard = (function(){
     //return { getBoard }
 });
 gameboard();
+//const board = gameboard().getBoard();
 
-//draw the gameboard to ui
-//ability to put symbols inside the gameboard spots
-//logic to decide turn
-//logic to decide symbol
-//logic to prevent collision
 //decide winning conditions(array/check each conditions)
 //display element that shows winner and gets removed from the screen
 //add restart button to play new game
